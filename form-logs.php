5e7f0e2471386b56d6af59989ee427753c373c74
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-15 16:11:07 +0200

    test

--- form-logs.php
+++ form-logs.php
@@ -8,11 +8,15 @@
             if(file_exists('log.json')){
                 $json = file_get_contents("log.json");
                 $json_array = json_decode($json, TRUE);
-                foreach ($json_array as $key => $value) {
-                    $ine = '<p>' . $value["prenom"] . ' | ' . $value["objet"] . ' | ' . $value["message"] . ' | ' . $value["date"] . ' | ' . $value["format"] . '</p>';
-                    echo($line);
+                if(count($json_array) > 0){
+                    foreach ($json_array as $key => $value){
+                        $line = '<p>' . $value["prenom"] . ' | ' . $value["objet"] . ' | ' . $value["message"] . ' | ' . $value["date"] . ' | ' . $value["format"] . '</p>';
+                        echo($line);
+                    }
+                }else{
+                    echo("Pas encore de logs.");
                 }
-            }else {
+            }else{
                 echo("Il n'y a pas encore de fichier log...");
             }
         ?>
