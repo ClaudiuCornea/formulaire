5c419c5ad733b03963e67d15a2f65a8b60df86c0
Claudiu <claudiu.simion.cornea@gmail.com>
2018-08-15 15:35:50 +0200

    show_logs

---
+++ form-logs.php
@@ -0,0 +1,12 @@
+<?php
+    if(file_exists('log.json')){
+        $json = file_get_contents("log.json");
+        $json_array = json_decode($json, TRUE);
+        foreach ($json_array as $key => $value) {
+            $ine = '<p>' . $value["prenom"] . ' | ' . $value["objet"] . ' | ' . $value["message"] . ' | ' . $value["date"] . ' | ' . $value["format"] . '</p>';
+            echo($line);
+        }
+    } else {
+        echo("Il n'y a pas encore de fichier log...");
+    }
+?>
